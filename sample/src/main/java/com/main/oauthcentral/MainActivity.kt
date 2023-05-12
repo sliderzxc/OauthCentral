cefaca8ec7ebe1a0d2797de79439146d28f84216
vadwwxz <vadwwxz@gmail.com>
2023-05-12 18:15:05 +0300

    Created library module (oauthcentral)

--- sample/src/main/java/com/main/oauthcentral/MainActivity.kt
+++ sample/src/main/java/com/main/oauthcentral/MainActivity.kt
@@ -2,10 +2,13 @@ package com.main.oauthcentral
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.main.oauthcentral.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
